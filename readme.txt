8ed7fedafd44a3963856bc3fc45ddd83b6501aba
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2020-07-15 19:48:51 +0545

    Run Build and changed WPC to DC (WP Calculator > Dynamic Calculator

---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== Dynamic Calculator ===
+Contributors: krishnasuwal
+Tags: calculator, embed calculator
+Requires at least: 4.9
+Tested up to: 5.4
+Requires PHP: 5.6
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Allows you to embed calculator in your posts and pages using shortcode.
+
+== Description ==
+
+Embed calculator in your posts and pages using shortcode.
+
+DISCLAIMER: UI design was referenced from Google calculator.
+
+**FEATURES:**
+
+* Basic operations (Addition, Subtraction, Division, Multiplication)
+* Remainder (Character `R` has been used to indicate `Remainder` as the use of `%` symbol might conflict with percentage)
+* Insert multiple zeros (You can insert upto 4 zeros with one click)
+* Use of Brackets
+* Previously calculated answer can be referenced using `Ans` button
+* Separate buttons for AC (All Clear) and CE (Clear Entry)
+
+**SHORTCODE:**
+Use the shortcode `wp_calculator` to embed a calculator.
+
+== Changelog ==
+
+= 1.0.0 - 2020-xx-xx =
+* Initial Release
--- wp-calculator.php
+++ wp-calculator.php
@@ -1,45 +1,48 @@
 <?php
 /**
- * Plugin Name: WP Calculator
- * Plugin URI: http://wordpress.org/plugins/wp-calculator
+ * Plugin Name: Dynamic Calculator
+ * Plugin URI: https://wordpress.org/plugins/dynamic-calculator/
  * Description: Embed calculators on your website.
  * Version: 1.0.0
- * Author: Godson Programmer
+ * Author: Krishna Suwal
  * Author URI: https://krishna-suwal.com
- * Text Domain: wp-calculator
- * License: MIT
+ * Text Domain: dynamic-calculator
+ * Requires PHP: 5.6
+ * Requires at least: 4.9
+ * License: GPL-2.0+
+ *
+ * @package Dynamic Calculator
  */
 
+if ( defined( 'DC_ENV_DEV' ) && DC_ENV_DEV ) {
+	define( 'DC_JS_DIR_URL', 'http://localhost:3000/static/js' );
+	define( 'DC_CSS_DIR_URL', 'http://localhost:3000/static/css' );
 
-if ( defined( 'WPC_ENV_DEV' ) && WPC_ENV_DEV ) {
-	define( 'WPC_JS_DIR_URL', 'http://localhost:3000/static/js' );
-	define( 'WPC_CSS_DIR_URL', 'http://localhost:3000/static/css' );
-
-	define( 'WPC_MAIN_JS_ASSET_FILENAME', 'main.js' );
-	define( 'WPC_MAIN_CSS_ASSET_FILENAME', 'main.css' );
+	define( 'DC_MAIN_JS_ASSET_FILENAME', 'main.js' );
+	define( 'DC_MAIN_CSS_ASSET_FILENAME', 'main.css' );
 } else {
-	define( 'WPC_JS_DIR_URL', plugin_dir_url( __FILE__ ) . 'assets/js' );
-	define( 'WPC_CSS_DIR_URL', plugin_dir_url( __FILE__ ) . 'assets/css' );
+	define( 'DC_JS_DIR_URL', plugin_dir_url( __FILE__ ) . 'assets/js' );
+	define( 'DC_CSS_DIR_URL', plugin_dir_url( __FILE__ ) . 'assets/css' );
 
-	define( 'WPC_MAIN_JS_ASSET_FILENAME', 'main.971988b4045260232936.js' );
-	define( 'WPC_MAIN_CSS_ASSET_FILENAME', 'main.1152c84ad29460152ee3.css' );
+	define( 'DC_MAIN_JS_ASSET_FILENAME', 'main.9b934e47387ef658894e.js' );
+	define( 'DC_MAIN_CSS_ASSET_FILENAME', 'main.b5abd60d1875487f6a67.css' );
 }
 
-function render_wp_calculator( $atts ) {
+function render_dc_calculator( $atts ) {
 	// React Assets.
 	$react_js     = includes_url( 'js/dist/vendor/react.min.js' );
 	$react_dom_js = includes_url( 'js/dist/vendor/react-dom.min.js' );
 
 	// Local Assets.
-	$wpc_js_asset_url  = WPC_JS_DIR_URL . '/' . WPC_MAIN_JS_ASSET_FILENAME;
-	$wpc_css_asset_url = WPC_CSS_DIR_URL . '/' . WPC_MAIN_CSS_ASSET_FILENAME;
+	$dc_js_asset_url  = DC_JS_DIR_URL . '/' . DC_MAIN_JS_ASSET_FILENAME;
+	$dc_css_asset_url = DC_CSS_DIR_URL . '/' . DC_MAIN_CSS_ASSET_FILENAME;
 
-	wp_enqueue_script( 'wpc-react-js', $react_js, array(), '16.9.0', true );
-	wp_enqueue_script( 'wpc-react-dom-js', $react_dom_js, array(), '16.9.0', true );
-	wp_enqueue_script( 'wpc-js', $wpc_js_asset_url, array( 'wpc-react-js', 'wpc-react-dom-js' ), false, true );
-	wp_enqueue_style( 'wpc-css', $wpc_css_asset_url );
+	wp_enqueue_script( 'dc-react-js', $react_js, array(), '16.9.0', true );
+	wp_enqueue_script( 'dc-react-dom-js', $react_dom_js, array(), '16.9.0', true );
+	wp_enqueue_script( 'dc-js', $dc_js_asset_url, array( 'dc-react-js', 'dc-react-dom-js' ), false, true );
+	wp_enqueue_style( 'dc-css', $dc_css_asset_url );
 
-	return '<div id="wp-calculator-container"></div>';
+	return '<div id="dc-calculator-container"></div>';
 }
 
-add_shortcode( 'wp_calculator', 'render_wp_calculator' );
+add_shortcode( 'dc_calculator', 'render_dc_calculator' );
